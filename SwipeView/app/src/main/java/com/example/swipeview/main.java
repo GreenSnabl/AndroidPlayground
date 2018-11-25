9d351e81b33b2bcf730800b2b2b486e4070704b5
GreenSnabl <dennis.imhof@googlemail.com>
2018-11-26 00:37:07 +0100

    added app

---
+++ SwipeView/app/src/main/java/com/example/swipeview/main.java
@@ -0,0 +1,44 @@
+package com.example.swipeview;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class main extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.remote:
+                    mTextMessage.setText(R.string.remote);
+                    return true;
+                case R.id.options:
+                    mTextMessage.setText(R.string.options);
+                    return true;
+                case R.id.channels:
+                    mTextMessage.setText(R.string.channels);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = findViewById(R.id.message);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
