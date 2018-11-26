0ae83bebab96988aa9e0243b561b2d1802ba950d
GreenSnabl <dennis.imhof@googlemail.com>
2018-11-26 22:56:42 +0000

    added new project FragmentBottomNav

---
+++ FragmentBottomNav/app/src/main/java/com/example/snbl/fragmentbottomnav/RemoteFragment.java
@@ -0,0 +1,16 @@
+package com.example.snbl.fragmentbottomnav;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class RemoteFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_remote, container, false);
+    }
+}
---
+++ FragmentBottomNav/app/src/main/java/com/example/snbl/fragmentbottomnav/main.java
@@ -0,0 +1,50 @@
+package com.example.snbl.fragmentbottomnav;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class main extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new RemoteFragment()).commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch(item.getItemId()) {
+                        case R.id.nav_remote:
+                            selectedFragment = new RemoteFragment();
+                            break;
+                        case R.id.nav_settings:
+                            selectedFragment = new SettingsFragment();
+                            break;
+                        case R.id.nav_channels:
+                            selectedFragment = new ChannelsFragment();
+                            break;
+                        case R.id.nav_recorder:
+                            selectedFragment = new RecorderFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                    return true;
+                }
+            };
+
+}
